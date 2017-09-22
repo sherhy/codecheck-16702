18df7fb29767600b465b57b5abf923553112c747
hyunjin <hyunjin.jeong@toki.waseda.jp>
2017-09-22 12:53:21 +0900

    error

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import httplib
 
 def main(argv):
   # このコードは引数と標準出力を用いたサンプルコードです。
@@ -7,6 +8,11 @@ def main(argv):
   # ---
   # This is a sample code to use arguments and outputs.
   # Edit and remove this code as you like.
+	conn = httplib.HTTPConnection("http://challenge-server.code-check.io", 80)
+	# conn.request("GET", "/api/hash")
+	# conn.request("GET", "/api/hash?q=hoge")
+	for i, v in enumerate(argv):
+		conn.request("GET", "/api/hash?q="+v)
+		response = conn.getresponse()
 
-  for i, v in enumerate(argv):
-    print("argv[{0}]: {1}".format(i, v))
+		print response.read()
